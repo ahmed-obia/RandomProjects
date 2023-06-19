d2cf8f48e8837ed2443eeb2dded26f66da762ed0
Ahmed Alaa <137111680+ahmed-obia@users.noreply.github.com>
2023-06-20 01:41:40 +0300

    index.js

--- DiscordOAuthLogin/index.js
+++ DiscordOAuthLogin/index.js
@@ -1,1 +1,19 @@
+// web server
+const path = require('path');
+const express = require('express');
 
+
+const app = express();
+
+app.use('',express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+	return response.sendFile('index.html', { root: '.' });
+});
+
+app.get('/auth/discord', (request, response) => {
+	return response.sendFile('landing.html', { root: '.' });
+});
+
+const port = '53134';
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
